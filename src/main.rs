f200d4a7847833b005f44a0696f72e6ef59e0173
Stuart Squires <stuart.squires@faire.com>
2022-12-02 09:36:41 -0500

    Better structure for future days

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,15 @@
-use std::io;
+use std::fs;
 
-struct TopThree {
-    first: u32,
-    second: u32,
-    third: u32,
-}
-
-impl TopThree {
-    fn maybe_insert(&self, new: u32) -> TopThree {
-        if new > self.first {
-            return TopThree {
-                first: new,
-                second: self.first,
-                third: self.second,
-            };
-        } else if new > self.second {
-            return TopThree {
-                first: self.first,
-                second: new,
-                third: self.second,
-            };
-        } else if new > self.third {
-            return TopThree {
-                first: self.first,
-                second: self.second,
-                third: new,
-            };
-        }
-
-        return TopThree {
-            first: self.first,
-            second: self.second,
-            third: self.third,
-        };
-    }
-}
+mod day_1;
 
 fn main() {
-    let lines = io::stdin().lines();
+    let day_1_input = fs::read_to_string("inputs/day_1.txt").expect("Error reading file.");
+    let day_1_input = day_1_input.split('\n').collect();
 
-    let mut top_three = TopThree {
-        first: 0,
-        second: 0,
-        third: 0,
-    };
-    let mut current_elf_calories = 0;
-    for line in lines {
-        let line = line.unwrap();
-
-        if line.trim() == "" {
-            top_three = top_three.maybe_insert(current_elf_calories);
-            current_elf_calories = 0;
-            continue;
-        }
-
-        let calories: u32 = line.trim().parse().expect("Calories must be a number.");
-
-        current_elf_calories += calories;
-    }
-
-    top_three = top_three.maybe_insert(current_elf_calories);
-
-    println!(
-        "Answer: {}",
-        top_three.first + top_three.second + top_three.third
-    );
+    println!("===============================");
+    println!("Answers:");
+    println!("===============================");
+    println!("Day 1, Part 1: {}", day_1::solve_part_1(&day_1_input));
+    println!("Day 1, Part 2: {}", day_1::solve_part_2(&day_1_input));
+    println!("===============================");
 }
