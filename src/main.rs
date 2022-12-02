df7fea1ea0c1cc17a3da99806d33f467d3da2379
Stuart Squires <stuart.squires@faire.com>
2022-12-02 12:48:38 -0500

    Fixup answer output

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod day_2;
 
 fn main() {
     println!("===============================");
-    println!("Answers:");
+    println!("ANSWERS");
     println!("===============================");
 
     let day_1_input = fs::read_to_string("inputs/day_1.txt").expect("Error reading file.");
@@ -18,8 +18,8 @@ fn main() {
     let day_2_input = fs::read_to_string("inputs/day_2.txt").expect("Error reading file.");
     let day_2_input = day_2_input.split('\n').collect();
 
-    println!("Day 1, Part 1: {}", day_2::solve_part_1(&day_2_input));
-    println!("Day 1, Part 2: {}", day_2::solve_part_2(&day_2_input));
+    println!("Day 2, Part 1: {}", day_2::solve_part_1(&day_2_input));
+    println!("Day 2, Part 2: {}", day_2::solve_part_2(&day_2_input));
 
     println!("===============================");
 }
