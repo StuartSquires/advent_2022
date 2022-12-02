5a9aeb1e3767bd177702187c66a6470e3d178490
Stuart Squires <stuart.squires@faire.com>
2022-12-02 07:41:53 -0500

    Day 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,55 @@
 use std::io;
 
+struct TopThree {
+    first: u32,
+    second: u32,
+    third: u32,
+}
+
+impl TopThree {
+    fn maybe_insert(&self, new: u32) -> TopThree {
+        if new > self.first {
+            return TopThree {
+                first: new,
+                second: self.first,
+                third: self.second,
+            };
+        } else if new > self.second {
+            return TopThree {
+                first: self.first,
+                second: new,
+                third: self.second,
+            };
+        } else if new > self.third {
+            return TopThree {
+                first: self.first,
+                second: self.second,
+                third: new,
+            };
+        }
+
+        return TopThree {
+            first: self.first,
+            second: self.second,
+            third: self.third,
+        };
+    }
+}
+
 fn main() {
     let lines = io::stdin().lines();
 
-    let mut maximum_elf_calories = 0;
+    let mut top_three = TopThree {
+        first: 0,
+        second: 0,
+        third: 0,
+    };
     let mut current_elf_calories = 0;
     for line in lines {
         let line = line.unwrap();
 
         if line.trim() == "" {
-            if current_elf_calories > maximum_elf_calories {
-                maximum_elf_calories = current_elf_calories;
-            }
+            top_three = top_three.maybe_insert(current_elf_calories);
             current_elf_calories = 0;
             continue;
         }
@@ -19,13 +57,12 @@ fn main() {
         let calories: u32 = line.trim().parse().expect("Calories must be a number.");
 
         current_elf_calories += calories;
-
-        println!("got a line: {}", line)
     }
 
-    if current_elf_calories > maximum_elf_calories {
-        maximum_elf_calories = current_elf_calories;
-    }
+    top_three = top_three.maybe_insert(current_elf_calories);
 
-    println!("Answer: {}", maximum_elf_calories);
+    println!(
+        "Answer: {}",
+        top_three.first + top_three.second + top_three.third
+    );
 }
