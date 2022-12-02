2d9d719a330c09af1475fe707bbafd452664c215
Stuart Squires <stuart.squires@faire.com>
2022-12-02 12:43:01 -0500

    Day 2 Part 2

--- src/day_2.rs
+++ src/day_2.rs
@@ -26,7 +26,44 @@ pub fn solve_part_1(lines: &Vec<&str>) -> u32 {
             _ => panic!("Invalid move!"),
         };
 
-        total_score += get_shape_score(&ours) + get_outcome_score(&ours, &theirs);
+        let outcome = get_outcome(&ours, &theirs);
+        total_score += get_shape_score(&ours) + get_outcome_score(&outcome);
+    }
+
+    total_score
+}
+
+pub fn solve_part_2(lines: &Vec<&str>) -> u32 {
+    let mut total_score = 0;
+
+    for line in lines {
+        let line: Vec<&str> = line.trim().split(' ').collect();
+
+        // Handle an empty line at end of file.
+        if line.len() < 2 {
+            continue;
+        }
+
+        let theirs = line[0];
+        let target = line[1];
+
+        let theirs = match theirs {
+            "A" => Move::ROCK,
+            "B" => Move::PAPER,
+            "C" => Move::SCISSORS,
+            _ => panic!("Invalid move!"),
+        };
+
+        let target = match target {
+            "X" => Outcome::LOSE,
+            "Y" => Outcome::DRAW,
+            "Z" => Outcome::WIN,
+            _ => panic!("Invalid target!"),
+        };
+
+        let ours = get_move(&theirs, &target);
+
+        total_score += get_shape_score(&ours) + get_outcome_score(&target);
     }
 
     total_score
@@ -40,22 +77,50 @@ fn get_shape_score(ours: &Move) -> u32 {
     }
 }
 
-fn get_outcome_score(ours: &Move, theirs: &Move) -> u32 {
+fn get_outcome(ours: &Move, theirs: &Move) -> Outcome {
     match ours {
         Move::ROCK => match theirs {
-            Move::ROCK => DRAW_SCORE,
-            Move::PAPER => LOSE_SCORE,
-            Move::SCISSORS => WIN_SCORE,
+            Move::ROCK => Outcome::DRAW,
+            Move::PAPER => Outcome::LOSE,
+            Move::SCISSORS => Outcome::WIN,
         },
         Move::PAPER => match theirs {
-            Move::ROCK => WIN_SCORE,
-            Move::PAPER => DRAW_SCORE,
-            Move::SCISSORS => LOSE_SCORE,
+            Move::ROCK => Outcome::WIN,
+            Move::PAPER => Outcome::DRAW,
+            Move::SCISSORS => Outcome::LOSE,
         },
         Move::SCISSORS => match theirs {
-            Move::ROCK => LOSE_SCORE,
-            Move::PAPER => WIN_SCORE,
-            Move::SCISSORS => DRAW_SCORE,
+            Move::ROCK => Outcome::LOSE,
+            Move::PAPER => Outcome::WIN,
+            Move::SCISSORS => Outcome::DRAW,
+        },
+    }
+}
+
+fn get_outcome_score(outcome: &Outcome) -> u32 {
+    match outcome {
+        Outcome::WIN => 6,
+        Outcome::DRAW => 3,
+        Outcome::LOSE => 0,
+    }
+}
+
+fn get_move(theirs: &Move, target: &Outcome) -> Move {
+    match theirs {
+        Move::ROCK => match target {
+            Outcome::WIN => Move::PAPER,
+            Outcome::DRAW => Move::ROCK,
+            Outcome::LOSE => Move::SCISSORS,
+        },
+        Move::PAPER => match target {
+            Outcome::WIN => Move::SCISSORS,
+            Outcome::DRAW => Move::PAPER,
+            Outcome::LOSE => Move::ROCK,
+        },
+        Move::SCISSORS => match target {
+            Outcome::WIN => Move::ROCK,
+            Outcome::DRAW => Move::SCISSORS,
+            Outcome::LOSE => Move::PAPER,
         },
     }
 }
@@ -66,6 +131,8 @@ enum Move {
     SCISSORS,
 }
 
-const WIN_SCORE: u32 = 6;
-const DRAW_SCORE: u32 = 3;
-const LOSE_SCORE: u32 = 0;
+enum Outcome {
+    WIN,
+    DRAW,
+    LOSE,
+}
--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,8 @@ fn main() {
     let day_2_input = fs::read_to_string("inputs/day_2.txt").expect("Error reading file.");
     let day_2_input = day_2_input.split('\n').collect();
 
-    println!("===============================");
     println!("Day 1, Part 1: {}", day_2::solve_part_1(&day_2_input));
+    println!("Day 1, Part 2: {}", day_2::solve_part_2(&day_2_input));
+
+    println!("===============================");
 }
